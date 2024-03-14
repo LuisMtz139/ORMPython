cfee599fe2dc5e94eb8ad171e653604f14647b99
Luis Martinez <LuisMartinezMA12@gmail.com>
2024-03-14 10:46:27 -0600

    funcional

--- app/database.py
+++ app/database.py
@@ -2,9 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:13960@localhost/prueba"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://doadmin:AVNS_zOQUWFt21cxvigclv9x@db-mysql-nyc3-80012-do-user-15899616-0.c.db.ondigitalocean.com:25060/defaultdb"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'ssl_ca': '/path/to/ca.pem'})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
